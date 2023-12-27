fedc9e3740bb67521a7ae74d11412cd7de607e5e
Viljami <viljami.irri@tuni.fi>
2023-12-27 18:40:00 +0200

    tictactoe class

--- SuperTicTacToe/tictactoe.cpp
+++ SuperTicTacToe/tictactoe.cpp
@@ -2,5 +2,66 @@
 
 TicTacToe::TicTacToe()
 {
+    board = Board();
+}
 
+Shape TicTacToe::isGameOver()
+{
+    std::vector<Cell> cells = board.getCells();
+
+    // Horizontal lines
+    int i = 0;
+    while(i < 9){
+        if(cells[i].getShape() == cells[i+1].getShape() &&
+            cells[i].getShape() == cells[i+2].getShape()){
+            if(cells[i].getShape() != Shape::empty){
+                gameIsOver = true;
+                return cells[i].getShape();
+            }
+        }
+        i += 3;
+    }
+
+    // Vertical lines
+    i = 0;
+    while( i < 3){
+        if(cells[i].getShape() == cells[i+3].getShape() &&
+            cells[i].getShape() == cells[i+6].getShape()){
+            if(cells[i].getShape() != Shape::empty){
+                gameIsOver = true;
+                return cells[i].getShape();
+            }
+        }
+        i++;
+    }
+
+    // Top-left to Bottom-right diagonal
+    if(cells[0].getShape() == cells[4].getShape() &&
+        cells[0].getShape() == cells[8].getShape()){
+        if(cells[i].getShape() != Shape::empty){
+            gameIsOver = true;
+            return cells[i].getShape();
+        }
+    }
+
+    // Top-right to Bottom-Left diagonal
+    if(cells[2].getShape() == cells[4].getShape() &&
+        cells[2].getShape() == cells[6].getShape()){
+        if(cells[i].getShape() != Shape::empty){
+            gameIsOver = true;
+            return cells[i].getShape();
+        }
+    }
+
+    return Shape::empty;
+}
+
+Board& TicTacToe::getBoard()
+{
+    return board;
+}
+
+bool TicTacToe::getIsGameOver()
+{
+    return gameIsOver;
 }
--- SuperTicTacToe/tictactoe.h
+++ SuperTicTacToe/tictactoe.h
@@ -1,11 +1,17 @@
 #ifndef TICTACTOE_H
 #define TICTACTOE_H
-
+#include "board.h"
 
 class TicTacToe
 {
 public:
     TicTacToe();
+    Shape isGameOver();
+    Board& getBoard();
+    bool getIsGameOver();
+private:
+    Board board;
+    bool gameIsOver = false;
 };
 
 #endif // TICTACTOE_H
