153f45036f28c69c464eb3d7231435871d700b02
Viljami <viljami.irri@tuni.fi>
2023-12-27 18:39:35 +0200

    board class

--- SuperTicTacToe/board.cpp
+++ SuperTicTacToe/board.cpp
@@ -2,5 +2,54 @@
 
 Board::Board()
 {
-
+    cells.reserve(9);
+    for(int i=0; i<9; i++){
+        cells.push_back(Cell());
+    }
+    for(auto& cell : cells){
+        cell.setShape(Shape::cross);
+    }
 }
+
+void Board::printBoard()
+{
+    for(int i=0; i<9; i++){
+        cells[i].printCell();
+        std::cout << " ";
+        if(i == 8){
+            return;
+        }
+        else if (i == 2 || i == 5){
+            std::cout << std::endl << "---------" << std::endl;
+        }
+        else{
+            std::cout << "| ";
+        }
+    }
+}
+
+Cell Board::getCell(int index)
+{
+    if (index < 9){
+        return cells[index];
+    }
+    else {
+        throw("Index out of bounds");
+    }
+}
+
+bool Board::setCell(int index, Shape shape)
+{
+    if(cells[index].getShape() == Shape::empty){
+        cells[index].setShape(shape);
+        return true;
+    }
+    return false;
+}
+
+std::vector<Cell>& Board::getCells()
+{
+    return cells;
+}
+
+
--- SuperTicTacToe/board.h
+++ SuperTicTacToe/board.h
@@ -1,11 +1,19 @@
 #ifndef BOARD_H
 #define BOARD_H
-
+#include <vector>
+#include "cell.h"
+#include <iostream>
 
 class Board
 {
 public:
     Board();
+    void printBoard();
+    Cell getCell(int index);
+    bool setCell(int index, Shape shape);
+    std::vector<Cell>& getCells();
+private:
+   std::vector<Cell> cells;
 };
 
 #endif // BOARD_H
