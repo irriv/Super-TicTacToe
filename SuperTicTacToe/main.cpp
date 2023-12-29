ce86a5c86bf6e913c00555db1f21b2b2da4bfdb1
Viljami <viljami.irri@tuni.fi>
2023-12-29 17:16:03 +0200

    main

--- SuperTicTacToe/main.cpp
+++ SuperTicTacToe/main.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main()
 {
-    SuperTicTacToe game = SuperTicTacToe();
-    game.play();
+    while (true){
+        SuperTicTacToe game = SuperTicTacToe();
+        std::cout << "SuperTicTacToe game starts!" << std::endl;
+        game.play();
+        std::string replay;
+        while (true){
+            std::cout << "Play again? (Y/N): ";
+
+            std::getline(std::cin, replay);
+            if(replay == "Y" || replay == "y"){
+                break;
+            }
+            else if(replay == "N" || replay == "n"){
+                break;
+            }
+            else{
+                std::cout << "Invalid input." << std::endl;
+            }
+        }
+        if(replay == "N" || replay == "n"){
+            break;
+        }
+    }
     return 0;
 }
